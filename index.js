0d54eb08f7890941b68c70caef88c0a05afc68fb
yasir <yasir.abdiweli@student.moringa.com>
2024-07-07 18:22:11 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const CONTAINER = document.getElementById("list_container")
+const INPUT = document.querySelector("#input")
+
+let shopping_items = []
+
+
+function renderItems() {
+
+    shopping_items.map((item) => {
+        const itemElement = document.createElement("li")
