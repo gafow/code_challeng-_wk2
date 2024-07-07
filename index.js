1398be1cf3b06787e00d92fbafd6fcce1b226ebd
yasir <yasir.abdiweli@student.moringa.com>
2024-07-07 18:26:31 +0300

    finished

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function renderItems() {
 
     shopping_items.map((item) => {
         const itemElement = document.createElement("li")
+
+        itemElement.innerHTML = `
+        <div >
+        <p class=${item.isPurchased && 'isPurchased'}>${item.name}</p>
+        <button class=${item.isPurchased && 'btn_purchased'} onclick="purchasedItem('${item.name}')">${item.isPurchased ? 'Purchased' : 'Purchase'}</button>
+        </div>
+        `
+        CONTAINER.appendChild(itemElement)
+    })
+}
+
